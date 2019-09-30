437a91f078c42002a8cf1a0d70f3258af4c058ab
Thaylan Conceição <tc.thaylan@gmail.com>
2019-09-30 01:02:53 -0300

    jwt adicionado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth')
+
 const router = express.Router();
 
+function generateToken (params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  })
+}
+
 router.post('/register', async (req, res) => {
+  const { email } = req.body;
   try {
+    if (await User.findOne({ email })) 
+      res.status(400).send({ error: 'Este email já existe' });
+    
     const user = await User.create(req.body);
-    return res.send({ user });
+
+    user.password = undefined;
+
+    return res.send({ 
+      user,
+      token: generateToken({ id: user.id })
+    });
   } catch (error) {
-    return res.status(400).send({ error: 'Registration failed' });
+    return res.status(400).send({ error: 'error' });
   }
+});
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user)
+    res.status(400).send('Este usuário não existe');
+
+  if (!await bcrypt.compare(password, user.password))
+    res.status(400).send('Senha inválida')
+
+  user.password = undefined;
+
+  return res.send({ 
+    user,
+    token: generateToken({ id: user.id }),
+   })
 });
 
 module.exports = (app) => app.use('/auth', router);
